35202d7155cc3fdbd84738117b162e959975a4f0
arielmazor <ariel.mazor.mail@gmail.com>
2022-04-27 10:15:44 +0300

    add App.tsx

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainContainer from './src/main.container'
+import Navigator from './src/'
+
+export default function App() {
+  return (
+    <MainContainer>
+      <Navigator />
+    </MainContainer>
+  )
+}
